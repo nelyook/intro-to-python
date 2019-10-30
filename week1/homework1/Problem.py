e94d2095d4d0e6ee850f59c872fde4c4b272ad77
nelyook <56783377+nelyook@users.noreply.github.com>
2019-10-31 00:07:16 +0400

    homeweork1

---
+++ week1/homework1/Problem.py
@@ -0,0 +1,18 @@
+
+print ('project cake')
+print ('difficulty 1')
+ingredients= ["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+flour= "175"
+butter= "175"
+sugar= "100g"
+eggs= "2"
+cocoa_powder= "1ts"
+baking_powder= "0.5"
+print ("flour - " + flour + "g")
+print ("butter - " + butter + "g")
+print ("sugar - " + sugar)
+print ("eggs - " + eggs + "pieces")
+print ("cocoa_powder - " + cocoa_powder)
+print ("baking_powder - " + baking_powder + "ts")
+
+
